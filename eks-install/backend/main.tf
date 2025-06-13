c95f46e9c70969be553402ae98cf8faaf66b88ff
sarsatis <sarthak8055@gmail.com>
2025-06-13 19:37:40 +0100

    changing bucket name

--- eks-install/backend/main.tf
+++ eks-install/backend/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "demo-terraform-eks-state-s3-bucket-541993"
+  bucket = "demo-terraform-eks-state-s3-bucket-2104"
 
   lifecycle {
     prevent_destroy = false
